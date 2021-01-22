a82e6803e674bc2662efb8396ad518f7d2e7d95c
ogadra <ogaryu0529@gmail.com>
2021-01-22 15:16:58 +0900

    pattern matching bug fixed

--- wordDifferenceRate.py
+++ wordDifferenceRate.py
@@ -4,12 +4,14 @@ import MeCab
 mecab = MeCab.Tagger('-Owakati')
 
 text = open('hitofusano_budo.txt').read()
-text = re.sub('［.*］', '', text)
-text = re.sub('《.*》', '', text)
+text = re.sub('［[^［］]*］', '', text)
+text = re.sub('《[^《》]*》', '', text)
 text = re.sub('\n| |　', '', text)
 text = mecab.parse(text)
+
+print(text)
 
 textList = text.split(' ')
 textSet = set(textList)
 print(len(textSet) / len(textList))
-# 0.26256281407035176
+# 0.1991095720999258
